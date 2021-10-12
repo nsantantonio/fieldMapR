5880d2b3197eff7f168947490412cf1326d04c42
Nicholas Santantonio <ns722@cornell.edu>
2021-10-12 15:58:38 -0400

    read GPS from google takeout, redundant with readGoogleGPS.R

---
+++ R/takeout.R
@@ -0,0 +1,19 @@
+#' GPStoDist function
+#'
+#' function to (do something)
+#'
+#' @param takeoutFile path to google takeout csv file.
+#' @return [value]
+#' @details [fill in details here]
+#' @examples none
+#' @export
+takeout <- function(takeoutFile, rowOrder = NULL){
+	coord <- read.csv(takeoutFile)
+	txtCoords <- strsplit(gsub(".*/", "", coord$URL), ",")
+
+	if(is.null(rowOrder)) rowOrder <- 1:nrow(coord)
+	boundries <- do.call(rbind, lapply(txtCoords, as.numeric))[rowOrder,c(2,1)]
+	colnames(boundries) <- c("lon", "lat")
+	# fieldPoly <- rbind(boundries, boundries[1,])
+	return(boundries)
+}
