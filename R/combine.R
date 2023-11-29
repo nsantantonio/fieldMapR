3f1bfebab08c0e0f3bc1822d60afd8f6bf2712f9
nsant <nsantantonio@gmail.com>
2023-11-29 13:44:22 -0500

    add rep when combining

--- R/combine.R
+++ R/combine.R
@@ -18,6 +18,7 @@ combine <- function(x1, x2, pile = FALSE) {
 	if (pile) {
 		mat <- combineMat(x1, x2)
 		fillmat <- combineMat(x1, x2, sname = "fill")
+		repmat <- combineMat(x1, x2, sname = "Rep")
 		entmat <- combineMat(x1, x2, sname = "Entry")
 		linemat <- combineMat(x1, x2, sname = "Line")
 		pedmat <- combineMat(x1, x2, sname = "Pedigree")
@@ -28,6 +29,7 @@ combine <- function(x1, x2, pile = FALSE) {
 		stakes <- c(x1@needStake, x2@needStake)
 		mat <- rbind(x2@matrix, x1@matrix)
 		fillmat <- rbind(x2@fill, x1@fill)
+		repmat <- rbind(x2@fill, x1@Rep)
 		entmat <- rbind(x2@fill, x1@Entry)
 		linemat <- rbind(x2@fill, x1@Line)
 		pedmat <- rbind(x2@fill, x1@Pedigree)
@@ -42,6 +44,7 @@ combine <- function(x1, x2, pile = FALSE) {
 			   needStake = stakes,
 			   borderPasses = x1@borderPasses,
 			   trialName = trialName,
+			   Rep = repmat, 
 			   Entry = entmat, 
 			   Line = linemat,
 			   Pedigree = pedmat)
